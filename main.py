6de87dd1ab7f01881127a9a6502578846f705fe4
Frederic Triplett, Jr. <frederic.triplett@gmail.com>
2026-07-12 17:29:55 -0400

    Testing core logic

---
+++ main.py
@@ -0,0 +1,38 @@
+from pawpal_system import Task, Pet, Owner, Scheduler
+from datetime import date, datetime, time
+from typing import List, Optional
+
+def main():
+    freddie = Owner("Freddie")
+
+    pluto = Pet("Pluto", "dog")
+    morgana = Pet("Morgana", "cat")
+
+    # My pets are mine
+    freddie.add_pet(morgana)
+    freddie.add_pet(pluto)
+
+    # Pluto Tasks
+    walk_pluto = Task("Walk", 30, "medium", "daily", time(hour=18, minute=30)) # 6:30 pm  7/15/26 
+    pluto_vet_appt = Task("Vet Appointment for Pluto", 60, "urgent", "monthly", time(11, 30)) # 11:30 am 7/15/26 
+    
+    # Morgana Chores
+    cat_playtime = Task(description="Playtime", duration=10,  priority="medium", frequency="daily", deadline=time(hour=17, minute=30)) # 5:30 pm 7/15/26 
+    
+    nail_trimming = Task(description="Trim Nails", duration=15, priority="medium", frequency="monthly", deadline=time(hour=18, minute=00)) # 6:00 pm 7/15/26 
+
+    clean_litter = Task("Clean litter box", 15, "high", "daily", datetime(year=2026, month=7, day=15, hour=19, minute=30)) # 7:30 pm 7/15/26
+   
+    morgana_grooming_appt = Task("Grooming Appointment", 60, "urgent", "monthly", time(10, 30)) # 10:30 am 7/15/26 
+
+    pluto.add_task(walk_pluto)
+    pluto.add_task(pluto_vet_appt)
+
+    morgana.add_task(cat_playtime)
+    morgana.add_task(clean_litter)
+    morgana.add_task(morgana_grooming_appt)
+
+    schedule = Scheduler(freddie)
+    schedule.get_schedule()
+
+main()
---
+++ pawpal_system.py
@@ -0,0 +1,120 @@
+# This is the core logic behind PawPal+.
+from dataclasses import dataclass, field
+from typing import List, Optional
+from datetime import datetime
+
+@dataclass
+class Task:
+    """
+    Represents a pet care task (e.g., walk, feeding, meds, grooming).
+    """
+    description: str
+    duration: int
+    priority: str
+    frequency: str
+    deadline: datetime
+    completion_status: bool = False
+
+    def is_complete(self) -> bool:
+        """Returns True if the task is complete, False otherwise."""
+        return self.completion_status
+    
+@dataclass
+class Pet:
+    """
+    Represents a pet, containing identifying info and a list of tasks.
+    """
+    name: str
+    species: str
+    tasks: List[Task] = field(default_factory=list)
+
+    def add_task(self, task: Task) -> None:
+        """Adds a task to the pet's list of tasks."""
+        self.tasks.append(task)
+
+    def get_tasks(self) -> List[Task]:
+        """Returns the list of tasks for this pet."""
+        return self.tasks
+
+    def update_task(self, task: Task) -> None:
+        """Updates an existing task."""
+        pass
+
+    def delete_task(self, task: Task) -> None:
+        """Deletes a task from the pet's list of tasks."""
+        if task in self.tasks:
+            self.tasks.remove(task)
+
+class Owner:
+    """
+    Represents a pet owner, containing identifying info and a list of pets.
+    """
+    def __init__(self, name: str):
+        self.name: str = name
+        self.pets: List[Pet] = []
+
+    def update_owner(self) -> None:
+        """Updates the owner's details."""
+        pass
+
+    def add_pet(self, pet: Pet) -> None:
+        """Adds a pet to the owner's list of pets."""
+        self.pets.append(pet)
+
+    def get_pet(self, name: str) -> Optional[Pet]:
+        """Retrieves a pet by name."""
+        for pet in self.pets:
+            if pet.name == name:
+                return pet
+        return None
+
+    def update_pet(self, pet: Pet) -> None:
+        """Updates a pet's information."""
+        pass
+
+    def delete_pet(self, pet: Pet) -> None:
+        """Deletes a pet from the owner's list of pets."""
+        if pet in self.pets:
+            self.pets.remove(pet)
+
+
+class Scheduler:
+    """
+    Manages, retrieves, and organizes tasks across multiple pets for an owner.
+    """
+    def __init__(self, scheduler_admin: Owner):
+        self.scheduler_admin: Owner = scheduler_admin
+        self.pets: List[Pet] = []
+        self.schedule: List[Task] = []
+
+    def create_schedule(self, pets: List[Pet]) -> List[Task]:
+        """Generates a schedule based on the list of pets and their tasks."""
+        for pet in self.scheduler_admin.pets:
+            self.schedule.extend(pet.get_tasks())
+        return self.schedule
+
+    def get_schedule(self): #:
+        """Returns the current generated schedule."""
+        print("Today's Schedule")
+        for pet in self.scheduler_admin.pets:
+            pet_tasks = {pet.name: pet.get_tasks()}
+        
+        for pet in pet_tasks:
+            print(f"{pet} - {pet[1].description}")
+
+
+    def update_pet(self, pet: Pet) -> None:
+        """Updates the schedule with the latest info for a pet."""
+        pass
+
+    def get_tasks(self, pets: List[Pet]) -> List[Task]:
+        """Gathers all tasks for the list of pets."""
+        return []
+
+    def sort_tasks(self, pets: List[Pet]) -> List[Task]:
+        """Sorts tasks by criteria such as priority, duration, or frequency."""
+        return []
+
+    def filter_tasks(self, pets: List[Pet]) -> List[Task]:
+        """Filters tasks based on criteria or constraints."""
+        return []
